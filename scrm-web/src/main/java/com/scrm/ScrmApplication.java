9f0f59f77804bfa52fd0f8ff97851796a57b6925
liuzhengyang <liuzhengyang@mockuai.com>
2021-09-26 10:37:48 +0800

    修改: 优化项目启动打印

--- scrm-web/src/main/java/com/scrm/ScrmApplication.java
+++ scrm-web/src/main/java/com/scrm/ScrmApplication.java
@@ -12,6 +12,12 @@ public class ScrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScrmApplication.class);
-        log.info("成功启动 +++++++");
+        log.info("(♥◠‿◠)ﾉﾞ  scrm启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                "  _____  _____ _____  __  __ \n" +
+                " / ____|/ ____|  __ \\|  \\/  |\n" +
+                "| (___ | |    | |__) | \\  / |\n" +
+                " \\___ \\| |    |  _  /| |\\/| |\n" +
+                " ____) | |____| | \\ \\| |  | |\n" +
+                "|_____/ \\_____|_|  \\_\\_|  |_|");
     }
 }
